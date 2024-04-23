9b6e9c613c77d8ce77fb343f7054769a93f9cc0f
Gantay <24ckn.187@gmail.com>
2024-04-23 23:32:54 +0300

    pading stats :)

--- store_test.go
+++ store_test.go
@@ -10,11 +10,15 @@ func TestPathTransformFunc(t *testing.T) {
 	key := "mybestpicture"
 	pathname := CASPathTransformFunc(key)
 	fmt.Println(pathname)
+	expectedPathName := "be17b/32c28/70b1c/0c73b/59949/db6a3/be781/4dd23"
+	if pathname != expectedPathName {
+		t.Errorf("Have %s want %s", pathname, expectedPathName)
+	}
 }
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
