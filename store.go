73fdd7434ec3cf82c7ce05f4524ffc3e2e4ad468
Gantay <24ckn.187@gmail.com>
2024-05-04 15:39:02 +0300

    YAY?

--- store.go
+++ store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
@@ -66,6 +67,17 @@ func (s *Store) readStream(key string) (io.ReadCloser, error) {
 
 	return os.Open(pathKey.FullPath())
 
+}
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, err
 }
 
 func (s *Store) writeStream(key string, r io.Reader) error {
